8ba3435729d15f1021533e770319e9e8163089ad
Chimi504 <lhamchimi312@gmail.com>
2025-03-28 21:37:39 +0600

    Practical 2

--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@ const pgp = require('pg-promise')();
 require('dotenv').config();
 
 const db = pgp({ 
-    host: process.env.DB_HOST | 'localhost', 
-    port: 5432, 
-    database: process.env.DB_NAME | 'bonlineBookStore',
-    user: process.env.DB_USER | 'postgres', 
-    password: process.env.DB_PASS | 'chimi@123' 
+    host: process.env.DB_HOST || 'localhost', 
+    port: process.env.DB_PORT || 5432, // Use process.env.DB_PORT if defined
+    database: process.env.DB_NAME || 'OnlineBookStore_db',
+    user: process.env.DB_USER || 'postgres', 
+    password: process.env.DB_PASS || 'your_password' 
 }); 
+
 module.exports = db;
